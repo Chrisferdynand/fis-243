47f7172f3cfc2135e2b0914e67518711d22671c2
Chrisferdynand <46060670+Chrisferdynand@users.noreply.github.com>
2019-12-17 14:54:00 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function(){
             $('#minutos').html(event.strftime('%M'));
             $('#segundos').html(event.strftime('%S'));
         });
-        $('.cuenta-regresiva-proyecto').countdown('2019/12/16 08:00:00', function(event){
+        $('.cuenta-regresiva-proyecto').countdown('2019/12/18 15:00:00', function(event){
             $('#dias').html(event.strftime('%D'));
             $('#horas').html(event.strftime('%H'));
             $('#minutos').html(event.strftime('%M'));
